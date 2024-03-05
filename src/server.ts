74ed6b380e5f49778b1fde200c4ea395e39bc829
adamawalters <adamawalters@gmail.com>
2024-03-05 08:47:11 -0500

    updated main so it does not leverage queries router / mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 import app from "./app";
-import { connectToMongoDB } from  "./db/connection";
+//import { connectToMongoDB } from  "./db/connection";
 
 const port = process.env.PORT || 5001;
 
-async function startServer() {
-    try {
-        await connectToMongoDB();
-        app.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
-        });
-    } catch (error) {
-        console.error('Server error:', error);
-    }
-}
 
-startServer();
-
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 

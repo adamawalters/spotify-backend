2598d37c9a315fd7bec165afe9280925f1fd3b3d
adamawalters <adamawalters@gmail.com>
2024-03-13 21:12:41 -0400

    added validation to queries controller for post (body) and get (query)

--- src/queries/queries.controller.ts
+++ src/queries/queries.controller.ts
@@ -2,9 +2,10 @@ import { NextFunction, Request, Response } from "express";
 import service from "./queries.service";
 import asyncHandler from "../errors/asyncHandler";
 import { Query } from "../utils/types";
+import { query, body, validationResult } from "express-validator";
 
 async function list(req: Request, res: Response) {
-    const { limit } = res.locals;
+    const { limit = 0 } = res.locals;
         const queries = await service.list(Number(limit));
         return res.json({ data: queries });
 }
@@ -15,17 +16,36 @@ async function post (req: Request, res: Response) {
     res.json({ data: await service.post({ search_keyword, artist_name, num_songs }) });
   } 
 
+const listValidations = [
+    query('limit').optional().isNumeric().toInt().withMessage('limit must be a number'),
+];
 
-function limitIsNumber(req: Request, res: Response, next: NextFunction) {
-    const { limit = 0 } = req.query;
-    if (isNaN(Number(limit))) {
-        next({ status: 400, message: "Limit must be a number" });
+const postValidations = [
+    body('data').exists().withMessage('data is required').isObject().withMessage('data must be an object'),
+    body('data.search_keyword').exists().withMessage('search_keyword is required'),
+    body('data.artist_name').exists().withMessage('artist_name is required'),
+    body('data.num_songs').exists().withMessage('num_songs is required').isNumeric().withMessage('num_songs must be a number'),
+];
+
+const handleQueryValidationErrors = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next({
+        status: 400,
+        message: `Validation errors: ${errors.array().map((error) => error.msg).join(", ")}`,
+      });
     }
-    res.locals.limit = limit;
+    // If no errors, then set the properties on res.locals from the query
+    for(const key in req.query) { 
+      res.locals[key] = req.query[key]
+    }
+
     next();
-}  
+  };
+
+
 
  export default {
-    list: [limitIsNumber, asyncHandler(list)],
-    post: asyncHandler(post),
+    list: [...listValidations, handleQueryValidationErrors, asyncHandler(list)],
+    post: [...postValidations, handleQueryValidationErrors, asyncHandler(post)],
  } 
